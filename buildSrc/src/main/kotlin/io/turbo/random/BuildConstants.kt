a22c7d759696f6260dcc9c1bad58cf5890480ac5
turbomaxee <137450788+TurboMaxe@users.noreply.github.com>
2026-03-03 23:02:52 +0500

    update

--- buildSrc/src/main/kotlin/io/turbo/random/BuildConstants.kt
+++ buildSrc/src/main/kotlin/io/turbo/random/BuildConstants.kt
@@ -2,9 +2,12 @@ package io.turbo.random
 
 import org.jetbrains.kotlin.konan.file.File
 
-object BuildConstants {
-     const val VERSION = "1.0.0"
-     const val API_VERSION = "1.21."
+object BuildConstants { 
+     val name = "TurboPlugin"
+     val main = "io.turbo.random.Main" 
+     val group = 
+     val ver = "1.0-SNAPSHOT"
+     val api = "1.21"
     
      val IS_SNAPSHOT: Boolean = VERSION.contains("SNAPSHOT")
 }
