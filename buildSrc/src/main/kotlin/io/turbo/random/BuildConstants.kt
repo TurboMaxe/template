6c190ce46e2c58a5b4808b7ad96df9063a105be1
turbomaxee <137450788+TurboMaxe@users.noreply.github.com>
2026-03-03 23:28:51 +0500

    a

--- buildSrc/src/main/kotlin/io/turbo/random/BuildConstants.kt
+++ buildSrc/src/main/kotlin/io/turbo/random/BuildConstants.kt
@@ -3,11 +3,16 @@ package io.turbo.random
 import org.jetbrains.kotlin.konan.file.File
 
 object BuildConstants { 
-     val name = "TurboPlugin"
-     val main = "io.turbo.random.Main" 
-     val group = 
+     // change immediately
+     val name = "Plugin"
+     // Main package class, expected to change this
+     val main = "io.turbo.random.Main"
+     // this is package, do not change
+     val group = "io.turbo.random"
+     // Change this when making big stuff, remove SNAPSHOT if it is not experimental
      val ver = "1.0-SNAPSHOT"
+     // Paper's api version
      val api = "1.21"
-    
+     // not-needed boolean, just determmines if the plugin is in snapshot
      val IS_SNAPSHOT: Boolean = VERSION.contains("SNAPSHOT")
 }
