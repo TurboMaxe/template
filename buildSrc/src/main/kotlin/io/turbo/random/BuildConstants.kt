d4b3e0b79172c2cd5f5df437cbca92c2f07aa63d
turbomaxee <137450788+TurboMaxe@users.noreply.github.com>
2026-03-06 14:56:43 +0500

    chore: update field

--- buildSrc/src/main/kotlin/io/turbo/random/BuildConstants.kt
+++ buildSrc/src/main/kotlin/io/turbo/random/BuildConstants.kt
@@ -5,14 +5,14 @@ import org.jetbrains.kotlin.konan.file.File
 object BuildConstants { 
      // change immediately
      val name = "Plugin"
-     // Main package class, expected to change this
-     val main = "io.turbo.random.Main"
      // this is package, do not change
      val group = "io.turbo.random"
      // Change this when making big stuff, remove SNAPSHOT if it is not experimental
      val ver = "1.0-SNAPSHOT"
      // Paper's api version
      val api = "1.21"
+     // class where bukkit's java plugin is extended
+     val main = "${group}.Main"
      // not-needed boolean, just determmines if the plugin is in snapshot
      val IS_SNAPSHOT: Boolean = VERSION.contains("SNAPSHOT")
 }
