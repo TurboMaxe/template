23f7f65050e61f6ce191c7a7dbc4863a15b9734c
turbomaxee <137450788+TurboMaxe@users.noreply.github.com>
2026-03-08 13:49:24 +0500

    refactor: comments and bool

--- buildSrc/src/main/kotlin/io/turbo/random/BuildConstants.kt
+++ buildSrc/src/main/kotlin/io/turbo/random/BuildConstants.kt
@@ -1,16 +1,12 @@
 package io.turbo.random
 
 object BuildConstants { 
-     // change immediately
-     val name = "Plugin"
-     // this is package, do not change
+     
+     val name = ""
      val group = "io.turbo.random"
-     // Change this when making big stuff, remove SNAPSHOT if it is not experimental
-     val ver = "1.0-SNAPSHOT"
-     // Paper's api version
+     val ver = "1.0-b1"
      val api = "1.21"
-     // class where bukkit's java plugin is extended
      val main = "${group}.Main"
-     // not-needed boolean, just determmines if the plugin is in snapshot
-     val IS_SNAPSHOT: Boolean = ver.contains("SNAPSHOT")
+     // is plugin beta?
+     val IS_BETA: Boolean = ver.contains("b")
 }
