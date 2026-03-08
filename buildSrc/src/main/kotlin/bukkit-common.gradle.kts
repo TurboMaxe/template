7a257b1a14f528dd4b425e87dee474ddaf6b5303
turbomaxee <137450788+TurboMaxe@users.noreply.github.com>
2026-03-08 13:50:46 +0500

    chore: add more dependencies

--- buildSrc/src/main/kotlin/bukkit-common.gradle.kts
+++ buildSrc/src/main/kotlin/bukkit-common.gradle.kts
@@ -6,3 +6,21 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    compileOnly("org.projectlombok:lombok:1.18.38")
+    annotationProcessor("org.projectlombok:lombok:1.18.38")
+
+    compileOnly("org.jetbrains:annotations:26.0.2")
+    annotationProcessor("org.jetbrains:annotations:26.0.2")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.12.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.12.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.12.2")
+    testCompileOnly("org.jetbrains:annotations:26.0.2")
+    testCompileOnly("org.projectlombok:lombok:1.18.38")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.38")
+
+
+}
+
