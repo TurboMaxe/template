775e5a00f6e4b0ee96347bc9118787ac72462f17
turbomaxee <137450788+TurboMaxe@users.noreply.github.com>
2026-03-08 15:32:04 +0500

    refactor: cleanup code

--- Bukkit/src/main/java/io/turbo/random/plugin/Plugin.java
+++ Bukkit/src/main/java/io/turbo/random/plugin/Plugin.java
@@ -3,41 +3,16 @@ package io.turbo.random.plugin;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.RegisteredServiceProvider;
-import lombok.Getter;
-import lombok.experimental.Accessors;
-import net.milkbowl.vault.economy.Economy;
 
-public class Plugin extends JavaPlugin {
+public final class Plugin extends JavaPlugin {
 @Getter public Plugin instance;
-@Getter @Accessors(fluent = true) public static Economy econ = null;
+
+ public Plugin() {
+  instance = this;
+ } 
   
  @Override
  public void onEnable() {
-    instance = this;
-    if (!setupEconomy()) {
-            getLogger().severe("Vault or an Economy provider was not found! Disabling Plugin.");
-            Bukkit.getPluginManager().disablePlugin(this);
-            return;
-        }
+    Bukkit.getLogger().info("Enabled plugin.")    
  }
-
-  @Override
-  public void onDisable() {
-     Bukkit.getConsoleSender().sendMessage("Disabling Plugin!"); 
- } 
-
-   private boolean setupEconomy() {
-        if (getServer().getPluginManager().getPlugin("Vault") == null) {
-           Bukkit.getConsoleSender().sendMessage("Could not find vault!");             
-           return false;
-        }
-        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
-        if (rsp == null) {
-           Bukkit.getConsoleSender().sendMessage("Could not fetch an economy provider!");
-            return false;
-        }
-        econ = rsp.getProvider();
-        return econ != null;
-  }  
 }
